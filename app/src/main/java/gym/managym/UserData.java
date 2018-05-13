6b8f840d2905fd52cd7188832ab0e438cc2d813e
KyungSuHyun <ase3238@gmail.com>
2018-05-13 16:02:45 +0900

    test

--- app/src/main/java/gym/managym/UserData.java
+++ app/src/main/java/gym/managym/UserData.java
@@ -11,7 +11,7 @@ public class UserData implements Parcelable {
     private String info3;
     private String info4;
 
-    //public UserData() { }
+    public UserData() { } //test
 
     public UserData(Parcel in) {
         readFromParcel(in);
